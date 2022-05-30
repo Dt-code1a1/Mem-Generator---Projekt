f73a1caa1e4288420de38cc70499c2c07394035d
Dt-code1a1 <danielt1113@gmail.com>
2022-05-30 13:22:05 +0200

    finisched version v1.0

--- app-generator/src/components/Meme.js
+++ app-generator/src/components/Meme.js
@@ -13,7 +13,7 @@ export default function Meme() {
             fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
             .then(data => (setAllMemes(data.data.memes)))
-    })
+    },[])
     
     function handleChange(event){
         const {name, value} = event.target
